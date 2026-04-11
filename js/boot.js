847c5ee8d9e56a27826b140b703cec47ba96a415
Kaisa <keaackerman@gmail.com>
2026-04-11 15:46:00 -0500

    Improved loader - Adds loading messages - Uses animated ellipsies so there is always constant movement - Added manual increase of percentages between two bounded values for long lasting functions

--- js/boot.js
+++ js/boot.js
@@ -9,7 +9,7 @@ import {
 } from "./navigation.js";
 import { setUpMobile } from "./setUpMobile.js";
 import { setUpMediaQueries } from "./setUpMediaQueries.js";
-import { setProgressBarPercent, toggleHomepageLoader } from "./homepageLoader.js";
+import { setProgressBarPercent, toggleHomepageLoader, moveBetweenPercent } from "./homepageLoader.js";
 import { setUpSearch } from "./searchFilters.js";
 import { setupClearFiltersBtn } from "./searchFilters.js";
 import { setupClearSearchInput } from "./searchFilters.js";
@@ -23,67 +23,79 @@ import { injectUserSettingsTemplate } from "./userSettings.js";
 import { injectTemplate } from "./uiHelpers.js";
 
 export async function boot() {
-  setProgressBarPercent(0);
+  setProgressBarPercent(0, "Loading...");
 
   toggleHomepageLoader(true);
 
-  setProgressBarPercent(5);
-
-  await injectUserSettingsTemplate();
+  setProgressBarPercent(5, "Restoring user settings...");
   
-  setProgressBarPercent(10);
+  await injectUserSettingsTemplate();
+
+  setProgressBarPercent(10, "Injecting icons...");
 
   await injectIconTemplates();
 
-  setProgressBarPercent(15);
-
+  setProgressBarPercent(15, "Setting up media queries...");
+  
   await setUpMediaQueries();
 
   setProgressBarPercent(20);
-  
+
+  moveBetweenPercent(20,30, "Loading SQL JS libraries...");
+
   const SQL = await loadSqlJs();
-  
-  setProgressBarPercent(70);
+
+  setProgressBarPercent(31);
+
+  moveBetweenPercent(32,80, "Initializing Database...");
 
   await initDatabase(SQL, "db/discobase.sqlite3");
-  
-  setProgressBarPercent(75);
+
+  setProgressBarPercent(81, "Fetching conversations...");
 
   buildConvoTreeAndRender();
 
-  setProgressBarPercent(80);
+  setProgressBarPercent(85, "Setting up components...");
 
   setUpNavigation();
+
   setUpFilterDropdowns();
+
   setupClearFiltersBtn();
+
   setUpMainHeader();
+
   setUpSearch();
+
   setupClearSearchInput();
+
   setUpMoreDetails();
+
   setupSearchInfiniteScroll();
+
   setUpSidebarToggles();
 
-  setProgressBarPercent(85);
+  setProgressBarPercent(88, "Preparing responsive view...");
 
   setUpMobile();
   
-  setProgressBarPercent(89);
+  setProgressBarPercent(91, "Setting up history management...");
 
   await setupBrowserHistory();
 
-  setProgressBarPercent(90);
+  setProgressBarPercent(92, "Handling initial URL navigation...");
 
   await handleInitialUrlNavigation();
 
-  setProgressBarPercent(92);
+  setProgressBarPercent(94, "Setting up conversation types modal...");
 
   await setupConversationTypesModal();
   
-  setProgressBarPercent(95);
+  setProgressBarPercent(95, "Injecting homepage container...");
   
   await injectTemplate("homepage.html", "homePageContainer");
 
-  setProgressBarPercent(100);
+  setProgressBarPercent(100, "Almost done...");
 
   toggleHomepageLoader(false);
 }
--- js/homepageLoader.js
+++ js/homepageLoader.js
@@ -1,15 +1,36 @@
 import { $, toggleElementVisibility } from "./uiHelpers.js";
 
+var loaderTextContainer = $("homepageLoaderTextContainer");
+var loadingText = $("homepageLoaderText");
 export function toggleHomepageLoader(isLoading) {
   // Homepage Loader
   const homepageLoader = $("homepageLoader");
   const homepageOverlay = $("homepageOverlay");
+  toggleElementVisibility(loadingText, isLoading);
+  toggleElementVisibility(loaderTextContainer, isLoading);
   toggleElementVisibility(homepageLoader, isLoading);
   toggleElementVisibility(homepageOverlay, isLoading);
 }
 
 const elem = $("progressBar");
-export function setProgressBarPercent(percentLoaded) {
+
+var width = 0;
+export function setProgressBarPercent(percentLoaded, text="Loading...") {
+  loadingText.setAttribute('data-loading-text', text);
+  width = Math.max(width, percentLoaded);
   elem.style.width = percentLoaded + "%";
   elem.innerHTML = percentLoaded + "%";
+}
+
+export function moveBetweenPercent(min, max, text="Loading...", delay = 250) {
+  var id = setInterval(frame, delay);
+  function frame() {
+    if (width >= max) {
+      clearInterval(id);
+      return;
+    } else {
+      width++;
+      setProgressBarPercent(width,text);
+    }
+  }
 }
